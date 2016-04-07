5965d5d5f9691da383623f98d0f5e451a237b546
Scott Humphries <git@sscotth.io>
2016-04-07 11:30:14 -0500

    Add SublimeText packages to installation

--- install.sh
+++ install.sh
@@ -234,8 +234,13 @@ npm cache clean
 apm stars --user sscotth --install
 yes | apm upgrade
 
-# Install Sublime Text 3 settings
+# Sublime Text 3
+# Symlink settings
 ln -fs ~/.dotfiles/SublimeText/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+# Install Package Control
+curl https://packagecontrol.io/Package%20Control.sublime-package > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
+# symlink requried packages
+ln -sf ~/.dotfiles/SublimeText/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 
 # Install Python apps
 # Upgrade pip
