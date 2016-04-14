5fc3934c73d90154e52f1e0b826f4f5b79cd367d
Scott Humphries <git@sscotth.io>
2016-04-14 12:40:29 -0500

    check for remaining .DS_Store files

--- lib/tasks/clean_ds_store.sh
+++ lib/tasks/clean_ds_store.sh
@@ -4,5 +4,6 @@
 
 clean_DS_Store () {
   sudo find / -name ".DS_Store" -depth -exec rm {} \;
+  ! sudo find / -name '.DS_Store' 2>/dev/null | grep .DS_Store
   # find . -type f -name '*.DS_Store' -ls -delete
 }
