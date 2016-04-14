6e49ef765f90148e7f7f8da25499d6b26574b029
Scott Humphries <git@sscotth.io>
2016-04-14 15:53:24 -0500

    fix SIP Enabled test

--- lib/tasks/ntfs.sh
+++ lib/tasks/ntfs.sh
@@ -4,12 +4,13 @@
 
 ntfs_prepare () {
   if csrutil status | grep "System Integrity Protection status: disabled"; then
-    echo "(SIP Enabled)" >&3
-
     if [ -f /sbin/mount_ntfs ]; then
       sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.original
     fi
     sudo ln -sf /usr/local/sbin/mount_ntfs /sbin/mount_ntfs
+  else
+    echo "(ERROR: SIP Enabled)" >&3
+    false
   fi
 
   my_sleep "${@}"
