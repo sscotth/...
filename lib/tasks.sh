4f23254b268e36c58f79021862d2859697c75f7c
Scott Humphries <git@sscotth.io>
2016-04-21 14:12:46 -0500

    use gtimeout because PATH may not be setup yet

--- lib/tasks.sh
+++ lib/tasks.sh
@@ -9,30 +9,30 @@ source ./lib/utilities.sh
 
 concurrent_install () {
     local args=(
-        - "Use NTP pool servers"                retry timeout 100 ./lib/tasks/osx/use_ntp_pool_servers.sh
-        - "Install OSX updates"                 retry timeout 600 ./lib/tasks/osx/install_osx_updates.sh
-        - "Load Mathias Bynens' Dotfiles"       retry timeout 100 ./lib/tasks/dotfiles/mathiasbynens_dotfiles_load.sh
-        - "Symlink Mathias Bynens' Dotfiles"    retry timeout 100 ./lib/tasks/dotfiles/mathiasbynens_dotfiles_symlink.sh
-        - "Load Personal Dotfiles"              retry timeout 100 ./lib/tasks/dotfiles/personal_dotfiles_load.sh
-        - "Symlink Personal Dotfiles"           retry timeout 100 ./lib/tasks/dotfiles/personal_dotfiles_symlink.sh
-        - "Generate SSH Keys"                   retry timeout 100 ./lib/tasks/osx/generate_ssh_keys.sh
-        - "Install Oh-My-ZSH"                   retry timeout 100 ./lib/tasks/osx/install_oh_my_zsh.sh
-        - "Load .zshrc from Oh-My-ZSH"          retry timeout 100 ./lib/tasks/osx/load_zshrc_from_oh_my_zsh.sh
-        - "Install ZSH Syntax Highlighting"     retry timeout 100 ./lib/tasks/osx/install_zsh_syntax_highlighting.sh
-        - "Install Node.js Version Manager"     retry timeout 100 ./lib/tasks/nodejs/install_node_version_manager.sh
-        - "Install Node.js Stable"              retry timeout 100 ./lib/tasks/nodejs/install_node_stable.sh
-        - "Install Node.js Stable Packages"     retry timeout 600 ./lib/tasks/nodejs/install_node_stable_packages.sh
-        - "Setup Node.js Stable"                retry timeout 100 ./lib/tasks/nodejs/setup_node_stable.sh
-        - "Install Node.js LTS"                 retry timeout 100 ./lib/tasks/nodejs/install_node_lts.sh
-        - "Install Node.js LTS Packages"        retry timeout 600 ./lib/tasks/nodejs/install_node_lts_packages.sh
-        - "Setup Node.js LTS"                   retry timeout 100 ./lib/tasks/nodejs/setup_node_lts.sh
-        - "Clean npm cache"                     retry timeout 100 ./lib/tasks/nodejs/clean_npm_cache.sh
-        - "Clean .DS_Store files"               retry timeout 100 ./lib/tasks/osx/clean_DS_Store.sh
-        - "Setup assistive devices"             retry timeout 100 ./lib/tasks/osx/setup_assistive_devices.sh
-        - "Install App Store apps"              retry timeout 100 ./lib/tasks/osx/install_app_store_apps.sh
-        - "Load OSX Settings"                   retry timeout 100 ./lib/tasks/osx/load_osx_defaults.sh
-        - "Prepare MongoDB"                     retry timeout 100 ./lib/tasks/osx/mongodb_prepare.sh
-        - "Prepare NTFS"                        retry timeout 100 ./lib/tasks/osx/ntfs_prepare.sh
+        - "Use NTP pool servers"                retry gtimeout 100 ./lib/tasks/osx/use_ntp_pool_servers.sh
+        - "Install OSX updates"                 retry gtimeout 600 ./lib/tasks/osx/install_osx_updates.sh
+        - "Load Mathias Bynens' Dotfiles"       retry gtimeout 100 ./lib/tasks/dotfiles/mathiasbynens_dotfiles_load.sh
+        - "Symlink Mathias Bynens' Dotfiles"    retry gtimeout 100 ./lib/tasks/dotfiles/mathiasbynens_dotfiles_symlink.sh
+        - "Load Personal Dotfiles"              retry gtimeout 100 ./lib/tasks/dotfiles/personal_dotfiles_load.sh
+        - "Symlink Personal Dotfiles"           retry gtimeout 100 ./lib/tasks/dotfiles/personal_dotfiles_symlink.sh
+        - "Generate SSH Keys"                   retry gtimeout 100 ./lib/tasks/osx/generate_ssh_keys.sh
+        - "Install Oh-My-ZSH"                   retry gtimeout 100 ./lib/tasks/osx/install_oh_my_zsh.sh
+        - "Load .zshrc from Oh-My-ZSH"          retry gtimeout 100 ./lib/tasks/osx/load_zshrc_from_oh_my_zsh.sh
+        - "Install ZSH Syntax Highlighting"     retry gtimeout 100 ./lib/tasks/osx/install_zsh_syntax_highlighting.sh
+        - "Install Node.js Version Manager"     retry gtimeout 100 ./lib/tasks/nodejs/install_node_version_manager.sh
+        - "Install Node.js Stable"              retry gtimeout 100 ./lib/tasks/nodejs/install_node_stable.sh
+        - "Install Node.js Stable Packages"     retry gtimeout 600 ./lib/tasks/nodejs/install_node_stable_packages.sh
+        - "Setup Node.js Stable"                retry gtimeout 100 ./lib/tasks/nodejs/setup_node_stable.sh
+        - "Install Node.js LTS"                 retry gtimeout 100 ./lib/tasks/nodejs/install_node_lts.sh
+        - "Install Node.js LTS Packages"        retry gtimeout 600 ./lib/tasks/nodejs/install_node_lts_packages.sh
+        - "Setup Node.js LTS"                   retry gtimeout 100 ./lib/tasks/nodejs/setup_node_lts.sh
+        - "Clean npm cache"                     retry gtimeout 100 ./lib/tasks/nodejs/clean_npm_cache.sh
+        - "Clean .DS_Store files"               retry gtimeout 100 ./lib/tasks/osx/clean_DS_Store.sh
+        - "Setup assistive devices"             retry gtimeout 100 ./lib/tasks/osx/setup_assistive_devices.sh
+        - "Install App Store apps"              retry gtimeout 100 ./lib/tasks/osx/install_app_store_apps.sh
+        - "Load OSX Settings"                   retry gtimeout 100 ./lib/tasks/osx/load_osx_defaults.sh
+        - "Prepare MongoDB"                     retry gtimeout 100 ./lib/tasks/osx/mongodb_prepare.sh
+        - "Prepare NTFS"                        retry gtimeout 100 ./lib/tasks/osx/ntfs_prepare.sh
 
         --require "Load Mathias Bynens' Dotfiles"
         --before  "Symlink Mathias Bynens' Dotfiles"
