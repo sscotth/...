521e2d4ac5da3e2cd9e257ed41123c5ae65b69b8
Scott Humphries <git@sscotth.io>
2016-07-31 20:16:08 -0500

    psudo

--- install.sh
+++ install.sh
@@ -12,39 +12,57 @@ if [ -z ${APPLE_ID_PASSWORD+x} ]; then
   echo ''
 fi
 
-if [ -z ${SUDOPASS+x} ]; then
-  read -s -p "SUDO Password:" SUDOPASS
-  export SUDOPASS=$SUDOPASS
-  echo ''
+function cached_psudo () {
+  if [ -z ${SUDOPASS+x} ]; then
+    read -s -p "SUDO Password:" SUDOPASS
+    export SUDOPASS=$SUDOPASS
+    echo ''
 
-  /usr/bin/expect <<EOD
-  set timeout 999
-  spawn sudo -v
-  expect "Password:"
-  send "$SUDOPASS\n"
-  expect eof
+    /usr/bin/expect <<EOD
+      set timeout 99999
+      spawn $@
+      expect "*?assword:*"
+      send "$SUDOPASS\n"
+      expect eof
 EOD
-fi
+  fi
+}
 
-sudo -v
+function cached_sudo () {
+  if [ -z ${SUDOPASS+x} ]; then
+    read -s -p "SUDO Password:" SUDOPASS
+    export SUDOPASS=$SUDOPASS
+    echo ''
+
+    /usr/bin/expect <<EOD
+      set timeout 99999
+      spawn sudo $@
+      expect "*?assword:*"
+      send "$SUDOPASS\n"
+      expect eof
+EOD
+  fi
+}
+
+cached_sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
-while true; do sudo -n true; sleep 6000; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 6000; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Update computer's time
-sudo ntpdate -u us.pool.ntp.org
+cached_sudo ntpdate -u us.pool.ntp.org
 
 # Never go into computer sleep mode
-sudo systemsetup -setcomputersleep Off > /dev/null
+cached_sudo systemsetup -setcomputersleep Off > /dev/null
 
 # Never dim display
-sudo pmset force -a displaysleep 0
+cached_sudo pmset force -a displaysleep 0
 
 # Disable screensaver
 defaults -currentHost write com.apple.screensaver idleTime 0
 
 echo "Install Homebrew if not installed"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+cached_psudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 
 echo "Run Homebrew Doctor"
 brew doctor
