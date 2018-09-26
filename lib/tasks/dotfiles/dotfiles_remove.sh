3e1491d35f8d7912e8a3c3d34fdef386d65dcd73
Scott Humphries <git@sscotth.io>
2018-09-25 22:59:10 -0500

    wip

--- lib/tasks/dotfiles/dotfiles_remove.sh
+++ lib/tasks/dotfiles/dotfiles_remove.sh
@@ -3,9 +3,10 @@
 # General dotfiles functions
 
 dotfiles_remove () {
+  echo "Find symlinks in the home directory"
+  find ~ -type l -maxdepth 1
   echo "Delete symlinks in the home directory"
-
-  find ~ -type l -d 1 -delete
+  find ~ -type l -maxdepth 1 -delete
 }
 
 dotfiles_remove
