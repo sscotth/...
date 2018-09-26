4928d593d61016c085b2401d467bbe6a8915fec1
Scott Humphries <git@sscotth.io>
2018-09-25 19:55:46 -0500

    wip

---
+++ lib/tasks/apps/iterm2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# General dotfiles functions
+
+iterm2 () {
+  echo "iterm2 shell integration"
+
+  curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
+}
+
+iterm2
