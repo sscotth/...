b4ed4ec00b629c6797d18d18f56170e88c591066
Scott Humphries <git@sscotth.io>
2018-09-25 22:47:58 -0500

    wip

---
+++ lib/tasks/dotfiles/personal_dotfiles.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+#
+# Personal dotfiles functions
+
+source ./lib/utilities.sh
+
+personal_dotfiles_load () {
+  echo "copy example files"
+  for file in $(find . -name '.*_example'); do
+    src=$file
+    dst=${file/_example/}
+
+    echo "$src ==> $dst"
+    cp -n $src $dst || true
+  done
+
+  cached_psudo sh .osx_supplement
+}
+
+personal_dotfiles_symlink_unique () {
+  echo "~/.gitignore_global ==> ~/.gitignore"
+  ln -sf ~/.gitignore_global ~/.gitignore
+
+  echo ".../.atom/ ==> ~/.atom/"
+  /bin/ln -shF ~/.dotfiles/.atom ~/.atom
+
+  echo ".../Brewfile ==> ~/Brewfile"
+  /bin/ln -shF ~/.dotfiles/Brewfile ~/Brewfile
+}
+
+personal_dotfiles_symlink_normal () {
+  for file in $(find . -name '.*' ! -name '.' \
+    ! -name '.git' ! -name '.gitignore' ! -name '.zshrc' \
+    ! -name '.logs' ! -path '*.atom*' ! -path '*math_dotfiles*'); do
+
+    src="$(pwd)/$(basename $file)"
+    dst="$HOME/$(basename $file)"
+
+    echo "$src ==> $dst"
+    ln -sf "$src" "$dst"
+  done
+}
+
+personal_dotfiles () {
+  personal_dotfiles_symlink_unique
+  personal_dotfiles_symlink_normal
+  personal_dotfiles_load
+}
+
+personal_dotfiles
