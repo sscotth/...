f7bb8a53a5e6e53feb341f9c0f5bad23a9cf8569
Scott Humphries <git@sscotth.io>
2018-09-25 21:56:15 -0500

    wip

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ defaults -currentHost write com.apple.screensaver idleTime 0
 # defaults write com.apple.touristd seed-https://help.apple.com/osx/mac/10.12/whats-new -date "$(date)"
 
 boxecho "Other concurrent tasks"
-cached_psudo ./lib/tasks/index.sh
+bash ./lib/tasks/index.sh
 
 exit 1
 
