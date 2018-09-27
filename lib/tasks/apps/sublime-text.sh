79dfa6f33178a54bd299c14852dc61c98c979df8
Scott Humphries <git@sscotth.io>
2018-09-27 17:00:00 -0500

    wip

--- lib/tasks/apps/sublime-text.sh
+++ lib/tasks/apps/sublime-text.sh
@@ -5,19 +5,26 @@ set -Eeoux pipefail
 source ./lib/utilities.sh
 
 sublime_text_plugins () {
-  boxecho "Remove old sublime-text settings"
-  rm -rf /Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  boxecho "Removing old sublime-text settings..."
+  echo "(Removing old sublime-text settings...)" >&3
+  rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+
   mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
 
-  boxecho "Symlink sublime-text settings"
-  ln -fs ~/.dotfiles/SublimeText/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-  ln -fs ~/.dotfiles/SublimeText/highlighter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  boxecho "Symlink sublime-text settings..."
+  echo "(Symlink sublime-text settings...)" >&3
+  ln -fs ~/.dotfiles/SublimeText/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+  ln -fs ~/.dotfiles/SublimeText/highlighter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/highlighter.sublime-settings
 
-  boxecho "Install Package Control"
+  boxecho "Installing package control..."
+  echo "(Installing package control...)" >&3
   curl https://packagecontrol.io/Package%20Control.sublime-package > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
 
-  boxecho "symlink requried packages"
-  ln -sf ~/.dotfiles/SublimeText/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  boxecho "Symlinking required packages..."
+  echo "(Symlinking required packages...)" >&3
+  ln -fs ~/.dotfiles/SublimeText/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 
   echo "(done)" >&3
 }
