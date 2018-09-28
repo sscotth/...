828f01d4e489033c88db85e6db8f6e65ae2f9cc6
Scott Humphries <git@sscotth.io>
2018-09-28 04:27:53 -0500

    wip

--- lib/tasks/macos/increase_ulimit.sh
+++ lib/tasks/macos/increase_ulimit.sh
@@ -9,7 +9,7 @@ source ./lib/utilities.sh
 increase_ulimit () {
   # https://unix.stackexchange.com/a/221988
 
-  cached_psudo ln -sf ~/.dotfiles/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
+  cached_sudo ln -sf ~/.dotfiles/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
   cached_sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
   cached_sudo chmod 0644 /Library/LaunchDaemons/limit.maxfiles.plist
 
