272bc39f529b2cd9b62e1b85ca4ec2d80eee7ead
Scott Humphries <git@sscotth.io>
2018-09-27 19:02:19 -0500

    wip

--- lib/tasks/dotfiles/personal_dotfiles.sh
+++ lib/tasks/dotfiles/personal_dotfiles.sh
@@ -17,7 +17,8 @@ personal_dotfiles_symlink_unique () {
 personal_dotfiles_symlink_normal () {
   for file in $(find . -name '.*' ! -name '.' \
     ! -name '.git' ! -name '.gitignore' ! -name '.zshrc' \
-    ! -name '.logs' ! -path '*.atom*' ! -path '*math_dotfiles*'); do
+    ! -name '.oh-my-zsh' ! -name '.logs' \
+    ! -path '*.atom*' ! -path '*math_dotfiles*'); do
 
     src="$(pwd)/$(basename $file)"
     dst="$HOME/$(basename $file)"
