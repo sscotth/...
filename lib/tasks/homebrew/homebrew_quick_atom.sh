124d294f499e1d77ee37a9f2e95b457287782dea
Scott Humphries <git@sscotth.io>
2018-09-27 23:49:22 -0500

    wip

--- lib/tasks/homebrew/homebrew_quick_atom.sh
+++ lib/tasks/homebrew/homebrew_quick_atom.sh
@@ -14,7 +14,7 @@ homebrew_quick_atom () {
   echo "($installed_versions)" >&3
 
   boxecho "jq"
-  ! command_exists jq && echo "Installing Atom" && brew install jq
+  ! command_exists jq && echo "Installing jq" && brew install jq
 
   installed_versions="$installed_versions, $(jq --version)"
   echo "($installed_versions)" >&3
