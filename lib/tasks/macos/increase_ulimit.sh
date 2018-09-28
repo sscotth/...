2d24338c30ef3642fd32985faeea89383302ca96
Scott Humphries <git@sscotth.io>
2018-09-28 04:27:22 -0500

    wip

--- lib/tasks/macos/increase_ulimit.sh
+++ lib/tasks/macos/increase_ulimit.sh
@@ -9,29 +9,7 @@ source ./lib/utilities.sh
 increase_ulimit () {
   # https://unix.stackexchange.com/a/221988
 
- local file='<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>limit.maxfiles</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>launchctl</string>
-      <string>limit</string>
-      <string>maxfiles</string>
-      <string>262144</string>
-      <string>524288</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>ServiceIPC</key>
-    <false/>
-  </dict>
-</plist>
-'
-
-  cached_psudo "echo $file | sudo tee /Library/LaunchDaemons/limit.maxfiles.plist"
+  cached_psudo ln -sf ~/.dotfiles/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
   cached_sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
   cached_sudo chmod 0644 /Library/LaunchDaemons/limit.maxfiles.plist
 
