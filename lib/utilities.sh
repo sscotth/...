bac13df36f76b5a6d4ca71de57dcf293c903ed19
Scott Humphries <git@sscotth.io>
2018-09-27 19:55:11 -0500

    wip

--- lib/utilities.sh
+++ lib/utilities.sh
@@ -40,7 +40,7 @@ mas_cli_signin () {
 
 brew_install_or_upgrade () {
   if brew ls --versions "$1" >/dev/null; then
-      HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"
+      HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1" || true
   else
       HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
   fi
