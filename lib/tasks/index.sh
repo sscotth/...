ad103a1aa1bead152140ea741886024ae7325af5
Scott Humphries <git@sscotth.io>
2018-09-27 22:18:25 -0500

    wip

--- lib/tasks/index.sh
+++ lib/tasks/index.sh
@@ -15,21 +15,20 @@ concurrent_install () {
 
         - "Homebrew cask"                       retry_command gtimeout --preserve-status 120 ./lib/tasks/homebrew/homebrew_cask.sh
         - "Homebrew zap"                        retry_command gtimeout --preserve-status 120 ./lib/tasks/homebrew/homebrew_zap.sh
-        - "Homebrew quick"                      retry_command gtimeout --preserve-status 120 ./lib/tasks/homebrew/homebrew_quick.sh
+        - "Homebrew quick"                      retry_command gtimeout --preserve-status 300 ./lib/tasks/homebrew/homebrew_quick.sh
         # - "Homebrew bundle"                     retry_command gtimeout --preserve-status 600 ./lib/tasks/homebrew/homebrew_bundle.sh
 
-        - "Remove previous Dotfiles"            retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/dotfiles_remove.sh
-        - "Mathias Bynens' Dotfiles"            retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/mathiasbynens_dotfiles.sh
-        - "Personal Dotfiles"                   retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/personal_dotfiles.sh
+        - "atom"                                retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/atom.sh
+        - "ruby"                                retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/ruby.sh
+        - "python"                              retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/python.sh
 
         - "Clean .DS_Store files"               retry_command gtimeout --preserve-status 300 ./lib/tasks/macos/clean_ds_store.sh
         - "Generate SSH Keys"                   retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/generate_ssh_keys.sh
         # - "Install Hosts file"                  retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/install_hosts_file.sh # Use Gas Mask app
-        - "Install Oh-My-ZSH"                   retry_command gtimeout --preserve-status 300 ./lib/tasks/macos/install_oh_my_zsh.sh
         - "Install macOS updates"               retry_command gtimeout --preserve-status 600 ./lib/tasks/macos/install_macos_updates.sh
+        - "Install Oh-My-ZSH"                   retry_command gtimeout --preserve-status 300 ./lib/tasks/macos/install_oh_my_zsh.sh
         - "Install ZSH Syntax Highlighting"     retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/install_zsh_syntax_highlighting.sh
         - "Load .zshrc from Oh-My-ZSH"          retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/load_zshrc_from_oh_my_zsh.sh
-
         - "Prepare MongoDB"                     retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/mongodb_prepare.sh
         - "Prepare NTFS"                        retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/ntfs_prepare.sh # Except for SIP check, SKIP FOR NOW
         - "Setup assistive devices"             retry_command gtimeout --preserve-status 120 ./lib/tasks/macos/setup_assistive_devices.sh
@@ -42,13 +41,14 @@ concurrent_install () {
         - "Install Node.js Stable Packages"     retry_command gtimeout --preserve-status 300 ./lib/tasks/nodejs/install_node_stable_packages.sh
         - "Clean npm cache"                     retry_command gtimeout --preserve-status 120 ./lib/tasks/nodejs/clean_npm_cache.sh
 
-        - "iterm2"                              retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/iterm2.sh
-        - "atom"                                retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/atom.sh
-        - "python"                              retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/python.sh
-        - "ruby"                                retry_command gtimeout --preserve-status 600 ./lib/tasks/apps/ruby.sh
-        - "sublime text"                        retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/sublime-text.sh
-        - "spoofmac"                            retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/spoofmac.sh
+        - "Remove previous Dotfiles"            retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/dotfiles_remove.sh
+        - "Mathias Bynens' Dotfiles"            retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/mathiasbynens_dotfiles.sh
+        - "Personal Dotfiles"                   retry_command gtimeout --preserve-status 120 ./lib/tasks/dotfiles/personal_dotfiles.sh
+
         - "asepsis"                             retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/asepsis.sh # Except for SIP check, SKIP FOR NOW
+        - "iterm2"                              retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/iterm2.sh
+        - "spoofmac"                            retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/spoofmac.sh
+        - "sublime text"                        retry_command gtimeout --preserve-status 120 ./lib/tasks/apps/sublime-text.sh
 
         --require "Remove previous Dotfiles"
         --before  "Mathias Bynens' Dotfiles"
