6d8b09c6e55464441d5b8cdd73a458ae8a042de7
Scott Humphries <git@sscotth.io>
2020-01-16 23:50:13 -0600

    Update installed Node.js versions

--- lib/tasks/nodejs/install_node_lts.sh
+++ lib/tasks/nodejs/install_node_lts.sh
@@ -13,27 +13,33 @@ install_node_lts () {
 
   local installed_versions=""
 
-  boxecho "Argon v4.x"
-  echo "(Installing Argon v4.x...)" >&3
-  nvm install --lts=Argon
-  installed_versions=$(node -v)
-  echo "($installed_versions)" >&3
+  # boxecho "Argon v4.x"
+  # echo "(Installing Argon v4.x...)" >&3
+  # nvm install --lts=Argon
+  # installed_versions=$(node -v)
+  # echo "($installed_versions)" >&3
 
-  boxecho "Boron v6.x"
-  echo "(Installing Boron v6.x...)" >&3
-  nvm install --lts=Boron
-  installed_versions="${installed_versions}, $(node -v)"
-  echo "($installed_versions)" >&3
+  # boxecho "Boron v6.x"
+  # echo "(Installing Boron v6.x...)" >&3
+  # nvm install --lts=Boron
+  # installed_versions="${installed_versions}, $(node -v)"
+  # echo "($installed_versions)" >&3
 
-  boxecho "Carbon v8.x"
-  echo "(Installing Carbon v8.x...)" >&3
-  nvm install --lts=Carbon
-  installed_versions="${installed_versions}, $(node -v)"
-  echo "($installed_versions)" >&3
+  # boxecho "Carbon v8.x"
+  # echo "(Installing Carbon v8.x...)" >&3
+  # nvm install --lts=Carbon
+  # installed_versions="${installed_versions}, $(node -v)"
+  # echo "($installed_versions)" >&3
 
   boxecho "Dubnium v10.x"
   echo "(Installing Dubnium v10.x...)" >&3
   nvm install --lts=Dubnium
+  installed_versions="${installed_versions}, $(node -v)"
+  echo "($installed_versions)" >&3
+
+  boxecho "Erbium v12.x"
+  echo "(Installing Erbium v12.x...)" >&3
+  nvm install --lts=Erbium
   installed_versions="${installed_versions}, $(node -v)"
   echo "($installed_versions)" >&3
 }
