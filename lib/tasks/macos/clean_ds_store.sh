648012d67b56350a4b052d82a99cc23b52a0d1dd
Scott Humphries <git@sscotth.io>
2020-01-19 18:15:57 -0600

    Force remove .DS_Store

--- lib/tasks/macos/clean_ds_store.sh
+++ lib/tasks/macos/clean_ds_store.sh
@@ -7,7 +7,7 @@ set -Eeoux pipefail
 source ./lib/utilities.sh
 
 clean_DS_Store () {
-  cached_sudo 'find / -name ".DS_Store" -depth -exec rm {} \;' || true
+  cached_sudo 'find / -name ".DS_Store" -depth -exec rm -f {} \;' || true
   # ! cached_sudo 'find / -name ".DS_Store" 2>/dev/null | grep .DS_Store'
   # find . -type f -name '*.DS_Store' -ls -delete
   echo "(done)" >&3
