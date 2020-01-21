9c6356dba6247a6072097f8425843b9f223e4f57
Scott Humphries <git@sscotth.io>
2020-01-20 18:59:17 -0600

    Install npm-check-updates

--- lib/tasks/nodejs/install_node_stable_packages.sh
+++ lib/tasks/nodejs/install_node_stable_packages.sh
@@ -14,56 +14,11 @@ install_node_stable_packages () {
   echo "(expo-cli)" >&3
   npm install -g expo-cli
 
+  echo "(npm-check-updates)" >&3
+  npm install -g npm-check-updates
+
   echo "(pure-prompt)" >&3
   npm install -g pure-prompt
-
-  # echo "(react-devtools)" >&3
-  # npm install -g react-devtools
-
-  # echo "(babel-eslint)" >&3
-  # npm install -g babel-eslint
-  # echo "(bower)" >&3
-  # npm install -g bower
-  # echo "(cordova)" >&3
-  # npm install -g cordova
-  # echo "(eslint)" >&3
-  # npm install -g eslint
-  # echo "(grunt-cli)" >&3
-  # npm install -g grunt-cli
-  # echo "(gulp)" >&3
-  # npm install -g gulp
-  # echo "(http-server)" >&3
-  # npm install -g http-server
-  # echo "(ionic)" >&3
-  # npm install -g ionic
-  # echo "(jshint)" >&3
-  # npm install -g jshint
-  # echo "(localtunnel)" >&3
-  # npm install -g localtunnel
-  # echo "(nativescript)" >&3
-  # npm install -g nativescript
-  # echo "(node-gyp)" >&3
-  # npm install -g node-gyp
-  # echo "(npm-check-updates)" >&3
-  # npm install -g npm-check-updates
-  # echo "(pm2)" >&3
-  # npm install -g pm2
-  # echo "(pnpm)" >&3
-  # npm install -g pnpm
-  # echo "(react-native-cli)" >&3
-  # npm install -g react-native-cli
-  # echo "(semistandard)" >&3
-  # npm install -g semistandard
-  # echo "(semistandard-format)" >&3
-  # npm install -g semistandard-format
-  # echo "(standard)" >&3
-  # npm install -g standard
-  # echo "(standard-format)" >&3
-  # npm install -g standard-format
-  # echo "(verdaccio)" >&3
-  # npm install -g verdaccio
-  # echo "(node-bin)" >&3
-  # npm install -g node-bin
 
   echo "(done)" >&3
 }
