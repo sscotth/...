b43aa7f1775c6de995a7ae764d28bc80b88f64d8
Scott Humphries <git@sscotth.io>
2020-01-20 18:24:22 -0600

    Full cask update on install

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ boxecho "Additional Homebrew fonts"
 brew search /nerd-font-mono/ | grep font | xargs brew cask install
 
 boxecho "Cask upgrade details"
-yes n | brew cu --no-brew-update && echo
+yes n | brew cu --all --force --no-brew-update --no-quarantine --cleanup && echo
 
 echo
 boxecho "Homebrew cleanup"
