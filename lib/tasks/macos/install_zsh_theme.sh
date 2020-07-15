bafd780ef8d4e9d1de9edd70507a8e4833fc54f8
Scott Humphries <git@sscotth.io>
2020-07-15 12:01:24 -0500

    ZSH_THEME installer

--- lib/tasks/macos/install_zsh_theme.sh
+++ lib/tasks/macos/install_zsh_theme.sh
@@ -8,11 +8,11 @@ source ./lib/utilities.sh
 
 install_zsh_theme () {
 
-  echo "(Clone the repository)" >&3
-  git_clone_or_pull https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k --depth 1
+  echo "(Link the homebrew)" >&3
+  ln -sf /usr/local/opt/powerlevel10k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
   echo "(Edit ZSH_THEME in .zshrc)" >&3
-
+  sed -i "" 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
   echo "(done)" >&3
 }
