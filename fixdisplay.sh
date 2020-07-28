6feeeb3df0a45f4993d711ae512b99a0bb2c9401
Scott Humphries <git@sscotth.io>
2020-07-28 10:55:54 -0500

    Make fixDisplay script executable

--- fixdisplay.sh
+++ fixdisplay.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 displayplacer "id:C2529D28-0275-0C0C-A4F1-ADA359B50E38 res:3440x1440 hz:100 color_depth:8 scaling:off origin:(0,0) degree:0" "id:60C276EA-875A-83AD-3121-F6BD9D283246 res:2160x3840 hz:60 color_depth:8 scaling:off origin:(-2160,-1764) degree:90" "id:CA63D786-2FE1-7637-76F8-55EFCA49DA7E res:3840x2160 hz:60 color_depth:8 scaling:off origin:(3440,-720) degree:0"
